a9fa13d2cb9cf08d3ca93529765630dec2d60d4d
Fernando Campos <32453254+fecampos@users.noreply.github.com>
2021-12-17 09:43:26 -0500

    Update recursive_prime.m

--- recursive_prime.m
+++ recursive_prime.m
@@ -1,6 +1,6 @@
 function [out] = prime_number(n)
 
-out = false; s = length(n);
+out = 'true'; s = length(n);
 
 if s == 1;
   n = [n round(n/2)+1];
@@ -9,13 +9,15 @@ end
 a = n(1); b = n(2);
 
 if b < 2
-  out = false; return
-elseif a == 2 | a == 3
-  out = true; return;
+  out = 'true'; return
+elseif a == 2 || a == 3
+  out = 'true'; return;
 elseif mod(a,b) == 0
-  out = true; return;
+  out = 'false'; return;
 else
   b = b-1; 
 end
 
 out = prime_number([a b]);
+
+return
